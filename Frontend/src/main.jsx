d4b8b7ffc18de38859af52ec472ad6977eabc0a6
mhd asjad <mhdasjad877@gmail.com>
2025-06-08 23:30:32 +0530

    final fix

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,16 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import {store , persistor } from './redux/store.js'
+import {store , persistor } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react';
 import { Toaster } from '@/components/ui/toaster'
 import 'leaflet/dist/leaflet.css'
 
 createRoot(document.getElementById('root')).render(
 
-
     <Provider store={store} >
-
       <PersistGate loading={null} persistor={persistor} >
         <App />
         <Toaster/>
